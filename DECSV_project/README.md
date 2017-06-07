20c8cd37d6c3286b68db10203bde65422dbf6a4c
Jaakko Tuuri <Jaakko.Tuuri@lut.fi>
2017-06-07 15:45:44 +0300

    Add project files.

---
+++ DECSV_project/README.md
@@ -0,0 +1,12 @@
+﻿# DECSV_project
+
+This project is about making a CSV file editor, that
+allows its users to open CSV files in it, anonymize personal
+data in it, tag keywords from the document or from premade
+list, and then save the edited text and keywords into new CSV file.
+
+The program in this project is made based on Electron framework. This
+allows the program to be built like a web application, and it also makes
+it possible to easily port the program to different platforms
+(Linux, Mac, Windows), thanks to the cross platform support of the
+Electron framework.
---
+++ DECSV_project/js/main.js
@@ -0,0 +1,64 @@
+﻿function updateImageUrl(image_id, new_image_url) {
+    var image = document.getElementById(image_id);
+    if (image)
+        image.src = new_image_url;
+}
+
+function createImage(image_id, image_url) {
+    var image = document.createElement("img");
+    image.setAttribute("id", image_id);
+    image.src = image_url;
+    return image;
+}
+
+window.onfocus = function () {
+    console.log("focus");
+    //focusWindow(true);
+}
+
+window.onblur = function () {
+    console.log("blur");
+    //focusWindow(false);
+}
+
+window.onresize = function () {
+    console.log("resize");
+    //updateContentStyle();
+    const remote = require('electron').remote;
+    const BrowserWindow = remote.BrowserWindow;
+    const focused_win = BrowserWindow.getFocusedWindow();
+
+    if (focused_win.isMaximized() && document.getElementById("win-maximize-restore-icon").src != "./assets/appbar.window.restore.png") {
+        document.getElementById("win-maximize-restore-icon").src = "./assets/appbar.window.restore.png";
+    }
+    else if (!focused_win.isMaximized() && document.getElementById("win-maximize-restore-icon").src != "./assets/appbar.app.png") {
+        document.getElementById("win-maximize-restore-icon").src = "./assets/appbar.app.png";
+    }
+    else {
+        //
+    }
+}
+
+window.onload = function () {
+    console.log("window loaded");
+
+    const remote = require('electron').remote;
+    const BrowserWindow = remote.BrowserWindow;
+    const focused_win = BrowserWindow.getFocusedWindow();
+
+    document.getElementById("win-minimize-icon").onclick = function () {
+        focused_win.minimize();
+        focused_win
+    }
+    document.getElementById("win-maximize-restore-icon").onclick = function () {
+        if (focused_win.isMaximized()) {
+            focused_win.unmaximize();
+        }
+        else {
+            focused_win.maximize();
+        }
+    }
+    document.getElementById("win-close-icon").onclick = function () {
+        focused_win.close();
+    }
+}
